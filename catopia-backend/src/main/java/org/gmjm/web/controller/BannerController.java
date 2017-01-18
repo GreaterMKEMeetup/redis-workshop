1ce3b0cd6d971b1955527a2e9de2abbc66c0e6a1
Andy Glassman <aglassman@digitalmeasures.com>
2017-01-18 01:32:20 -0600

    Created base for workshop start point.

--- catopia-backend/src/main/java/org/gmjm/web/controller/BannerController.java
+++ catopia-backend/src/main/java/org/gmjm/web/controller/BannerController.java
@@ -1,48 +1,41 @@
 package org.gmjm.web.controller;
 
 import java.io.IOException;
-import java.util.Optional;
-import java.util.Set;
 import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
 
-import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
 
-import org.gmjm.model.Banner;
 import org.springframework.beans.factory.annotation.Autowired;
-import static org.springframework.web.bind.annotation.RequestMethod.*;
-import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.data.redis.core.BoundValueOperations;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import static org.springframework.web.bind.annotation.RequestMethod.*;
 
 @RestController()
 public class BannerController
 {
 
-	@Resource(name="appRedisTemplate")
-	ValueOperations<String,String> valueOperations;
+	private BoundValueOperations<String,String> valueOperations;
 
-
-
+	@Autowired
+	public BannerController(RedisTemplate<String,String> redisTemplate)
+	{
+		this.valueOperations = redisTemplate.boundValueOps("banner");
+	}
 
 	@RequestMapping(path="/banner",method=GET, headers="Accept=text/plain")
 	public String getBanner(HttpServletResponse respone) throws IOException
 	{
-		 String banner = valueOperations.get("banner");
-
-		 Long remainingTime = valueOperations.getOperations().getExpire("banner");
-
-		 return banner;
+		 return valueOperations.get();
 	}
 
 	@RequestMapping(path="/banner",method=PUT)
 	public void putBanner(@RequestBody  String banner) throws IOException
 	{
-		valueOperations.set("banner",banner,60, TimeUnit.SECONDS);
+		valueOperations.set(banner,60, TimeUnit.SECONDS);
 	}
 
 }
